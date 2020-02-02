f8a87610c51d42056bc146c710fd64b7e1ac99e0
Johnny Souto <johnny.souto@icloud.com>
2020-02-02 17:30:01 -0300

    validacoes concluidas

--- src/www/js/scripts.js
+++ src/www/js/scripts.js
@@ -20,9 +20,14 @@ function showSuccess(input, message){
 }
 
 // Check email is valid
-function isValidEmail(email){
+function checkEmail(input){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    // return re.test(String(email).toLowerCase());
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'E-mail is not valid');
+    }
 }
 
 // Check required fields
@@ -34,6 +39,24 @@ function checkRequired(inputArr){
             showSuccess(input);
         }
     });
+}
+
+// Check Passwords Match
+function checkPasswordsMatch(input1, input2){
+    if (input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match');
+    }
+}
+
+// Check lenght
+function checkLength(input, min, max){
+    if(input.value.length < min){
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if(input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
 }
 
 // Get fieldname
@@ -74,4 +97,8 @@ form.addEventListener('submit', function(e){
     }
     */
    checkRequired([username, email, password, password2]);
+   checkLength(username, 3, 15);
+   checkLength(password, 6, 25);
+   checkEmail(email);
+   checkPasswordsMatch(password, password2);
 });
